b2fdf269baaf8684fdb0db347ebd48c372a8b950
arbaap <arbaadhipamungkas@gmail.com>
2024-02-09 19:37:08 +0700

    tambah data

--- sensor.js
+++ sensor.js
@@ -1,11 +1,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const cors = require("cors"); // Tambahkan modul CORS
+const cors = require("cors");
 
 const app = express();
-
-// Menghilangkan penggunaan opsi yang sudah tidak diperlukan
 
 var mongoURL =
   "mongodb+srv://uinjek:uinjek@cluster0.93sie2d.mongodb.net/kibidb";
@@ -31,19 +29,17 @@ const Sensor = mongoose.model("Sensor", sensorSchema);
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(cors()); // Tambahkan middleware CORS
+app.use(cors());
 
 app.post("/kirimdata", async (req, res) => {
   const { suhu, kelembaban, NH3 } = req.body;
 
-  // Membuat instance sensor baru
   const newSensor = new Sensor({
     suhu: suhu,
     kelembaban: kelembaban,
   });
 
   try {
-    // Menggunakan async/await untuk menyimpan data
     const result = await newSensor.save();
     console.log("Berhasil menyimpan data sensor:", result);
     res.status(200).json({ message: "Berhasil menyimpan data sensor" });
@@ -53,10 +49,39 @@ app.post("/kirimdata", async (req, res) => {
   }
 });
 
+app.post("/kirimdatatest", async (req, res) => {
+  const { suhu, kelembaban, NH3 } = req.body;
+
+  const newSensor = new SensorTest({
+    suhu: suhu,
+    kelembaban: kelembaban,
+    NH3: NH3,
+  });
+
+  try {
+    const result = await newSensor.save();
+    console.log("Berhasil menyimpan data sensor test:", result);
+    res.status(200).json({ message: "Berhasil menyimpan data sensor" });
+  } catch (err) {
+    console.log("Gagal menyimpan data sensor:", err);
+    res.status(500).json({ message: "Gagal menyimpan data sensor" });
+  }
+});
+
 app.get("/ambildata", async (req, res) => {
   try {
-    // Mengambil semua data sensor dari database
     const data = await Sensor.find({});
+    console.log("Berhasil mengambil data sensor");
+    res.status(200).json(data);
+  } catch (err) {
+    console.log("Gagal mengambil data sensor:", err);
+    res.status(500).json({ message: "Gagal mengambil data sensor" });
+  }
+});
+
+app.get("/ambildatatest", async (req, res) => {
+  try {
+    const data = await SensorTest.find({});
     console.log("Berhasil mengambil data sensor");
     res.status(200).json(data);
   } catch (err) {
@@ -72,7 +97,6 @@ app.get("/", async (req, res, next) => {
   });
 });
 
-// Menjalankan server pada port tertentu
 const port = 3000;
 app.listen(port, () => {
   console.log(`Server berjalan di port ${port}`);
