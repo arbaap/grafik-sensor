77b9d8a94e25e53eba674dff15ff35c628e7adb4
arbaap <arbaadhipamungkas@gmail.com>
2024-02-13 02:04:10 +0700

    add get /

--- sensor.js
+++ sensor.js
@@ -8,8 +8,15 @@ const sensorRoute = require("./routes/sensorRoute");
 
 app.use(express.json());
 
+app.get("/", async (req, res, next) => {
+  return res.status(200).json({
+    title: "Server Jalan",
+    message: "Server siap digunakan!",
+  });
+});
+
 app.use("/api/sensors", sensorRoute);
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log("Server Sensor is running on port", port));
